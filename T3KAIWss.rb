9abc14b610474ee02f06a1453e24be074e6ecfbd
chipcarlson <36241337+chipcarlson@users.noreply.github.com>
2022-03-22 15:49:09 -0400

    Update T3KAIWss.rb
    Updates to accommodate different item types

--- T3KAIWss.rb
+++ T3KAIWss.rb
@@ -47,8 +47,6 @@ def nuixWorkerItemCallback(worker_item)
 	t3kid = t3kaijsonstring["t3kid"]
 	t3kaibody = ''
 	pathtoimage = ''
-	nomatch_count = 0
-	match_count = 0
 
 	if analyzekindsarray.include?(item_kind.to_s)
 		binary = source_item.getBinary
@@ -99,6 +97,7 @@ def nuixWorkerItemCallback(worker_item)
 			puts "Response Body: #{responsebody}"
 			begin
 				if responsecode == '200'
+					
 					pollingitem = worker_item
 					responsebodyvalues = JSON.parse(responsebody)
 					responsebodyvalues.each do |responseid, responsepath|
@@ -136,28 +135,38 @@ def nuixWorkerItemCallback(worker_item)
 					detectionscount = detections.count
 					puts "Detections Count: #detectionscount"
 					if detectionscount == 0
-						nomatch_count += 1
+						nomatch_count +=1
 						pollingitem.addTag("T3KAI Detection|Nothing to Report")
-						puts "T3KAi Result: Nothing to Report"
 					else
 						resultsjson["detections"]["0"].each do |detection|
-							match_count += 1
-							if detectionvalues == ''
-								detectionvalues = "#{detection[0]} - #{detection[1]}"
-							else
-								detectionvalues = detectionvalues + "," + "#{detection[0]} - #{detection[1]}"
+						match_count += 1
+						puts "Detection : #{detection}"
+						if detection[0] == "None"
+							detectionvalues = detection[2]
+							detectionvalues.each do |detectionvalue|
+								detectiontype = detectionvalue[0]
+								detectionpercent = detectionvalue[1]
+								puts "Detection Type : #{detectiontype}"
+								puts "Detetion Percent : #{detectionpercent}"
+								if detectionvalues == ''
+									detectionvalues = "#{detectiontype} - #{detectionpercent}"
+								else
+									detectionvalues = detectionvalues + "," + "#{detectiontype} - #{detectionpercent}"
+								end
 							end
-							puts "Detection : #{detection}"
-							puts "Detection Type : #{detection[0]}"
+							pollingitem.addTag("T3KAI Detection|#{detectiontype}")
+							pollingitem.addCustomMetadata("t3kairesult", "Match Detected", "text", "api")
+							pollingitem.addCustomMetadata("t3kaidetection", "#{detetionvalues}", "text", "api")
+						else
+							detectiontype = detection[0]
+							detectionpercent = detection[1]
+							puts "Detection Value : #{detection[0]}"
 							puts "Detection Percent : #{detection[1]}"
-							pollingitem .addTag("T3KAI Detection|#{detection[0]}")
-							puts "T3KAI Detection Type: #{detection[0]} : perecent: #{detection[1]}"
-						end
-						pollingitem.addCustomMetadata("t3kairesult", "Match Detected", "text", "api")
-#						pollingitem.getCustomMetadata["t3kairesult"] = "Match Detected"
-						pollingitem.addCustomMetadata("t3kaidetection", "#{detectionvalues}", "text", "api")
-#						pollingitem.getCustomMetadata["t3kaidetection"] = "#{detectiontype} - #{detectionpercent}"
 
+							pollingitem.addTag("T3KAI Detection|#{detectiontype}")
+							pollingitem.addCustomMetadata("t3kairesult", "Match Detected", "text", "api")
+							pollingitem.addCustomMetadata("t3kaidetection", "#{detectiontype} - #{detectionpercent}", "text", "api")
+						end
 					end
 				elsif responsecode == '400'
 					puts "Response code #{responsecode}"
@@ -179,121 +188,4 @@ def nuixWorkerItemCallback(worker_item)
 			end
 		end
 	end
-#	worker_guid = worker_item.getWorkerGuid()
-#	CSV.open("C:\\Users\\CCarlson01\\Documents\\Nuix\\CrimesAgainstChildren\\Processing Files\\#{worker_guid}.csv", 'a+t')  do |csv|
-#		worker_item.setProcessItem(true)
-#		source_item = worker_item.getSourceItem
-#		
-#		item_type = source_item.type.name
-#		if item_type == "application/xml"
-#			source_item_text = ''
-#			source_item_get_text = source_item.getText.to_s
-#			source_item.getBinary.withBinaryData do |binary_data|
-#				input_stream = binary_data.getInputStream
-#				source_item_text = org.apache.commons.io.IOUtils.toString(input_stream,"UTF8")
-#			end
-#			doc = REXML::Document.new(source_item_text)
-#			doc.root.each_recursive do |elem|
-#				parent_node = elem.parent
-#				path = get_node_path(parent_node)
-#				elementvalue = elem.get_text
-#				elementvalue = elementvalue.to_s.strip
-#				if "#{elementvalue}" != nil && "#{elementvalue}".length < 256
-#					basepath = path.map{|n|n.name}.join("/")
-#					full_path = basepath + "/#{elem.name}"
-#					full_path = full_path[1..-1]
-#					full_path = full_path.gsub '/', '|'
-#					full_path = full_path.gsub '| ', '|'
-#					full_path = full_path.gsub ' | ', '|'
-#					basepath = basepath.gsub '/', '|'
-#					basepath = basepath.gsub '| ', '|'
-#					basepath = basepath.gsub ' |', '|'
-#					searchadditionaldatasql = "select ElementName from CACValues where ElementName = '|#{full_path}|' and SearchAdditionalData = -1"
-#					searchadditionaldata = db.query(searchadditionaldatasql)
-#					searchadditionaldata.each do |additionaldata|
-#						cobwebsEmailEndpoint = cobwebsURL + '&Email=' + "#{elem.name}"
-#						uri = URI(cobwebsEmailEndpoint)
-#						response = Net::HTTP.get(uri)
-#						cobwebsemailjson = JSON.parse(response)
-#
-#						cobwebsemailjson["persons"].each do |person|
-#						msp = person["matchScorePercent"]
-#						if msp == 1.0 
-#							puts "value matched"
-#						elsif msp == 0.0
-#							puts "value not matched"
-#						end
-#					end
-
-
-#					createtagsql = "select ElementName from CACValues where ElementName = '|#{full_path}|' and CreateTag = -1"
-#					tagrecords = db.query(createtagsql)
-#					tagrecords.each do |tagrecord|
-#						if elementvalue.to_s.strip != '' && elementvalue.to_s.strip != nil && elementvalue.to_s.strip.length < 256
-#							tag_path = full_path + '|' + elementvalue.to_s
-#							tag_path = tag_path.delete("\n")
-#							begin
-#								puts "Adding Tag 1 #{tag_path} to #{worker_item.getSourceItem.getName}"
-#								worker_item.addTag(tag_path)
-#								csv << ["tag","#{tag_path}",Time.now.strftime("%d/%m/%Y")]
-#							rescue
-#								puts "ERROR 3 - Adding Tag #{tag_path} to #{worker_item.getSourceItem.getName}"
-#							end
-#						end
-#						elem.attributes.each_attribute do |attrs| 
-#							if attrs.name.to_s.strip != '' && attrs.name.to_s.strip != nil && attrs.name.to_s.strip.length < 256
-#								if attrs.value.to_s.strip != '' && attrs.value.to_s.strip != nil && attrs.value.to_s.strip.length < 256
-#									full_path = path.map{|n|n.name}.join("/") + "/#{elem.name}"
-#									full_path = full_path[1..-1]
-#									full_path = full_path.gsub '/', '|'
-#									full_path = full_path.gsub '/', '|'
-#									full_path = full_path.gsub '| ', '|'
-#									full_path = full_path.gsub ' |', '|'
-#									full_path = full_path + "|" + "#{attrs.name}"
-#									tag_path = full_path + '|' + elementvalue.to_s + '|' + attrs.name + '|' + attrs.value.to_s.strip
-#									tag_path = tag_path.delete("\n")
-#									case_path = full_path.strip
-#									itemtag = "#{case_path}|#{attrs.value.to_s.strip}"
-#									full_tag = full_path + "|" + "#{attrs.value.to_s.strip}"
-#									begin
-#										puts "Adding Tag 2 #{tag_path} to #{worker_item.getSourceItem.getName}"
-#										worker_item.addTag(tag_path)
-#										csv << ["tag","#{tag_path}",Time.now.strftime("%d/%m/%Y")]
-#									rescue
-#										puts "ERROR 4 - Adding Tag #{tag_path} to #{worker_item.getSourceItem.getName}"
-#									end
-#								end
-#							end
-#						end
-#					end
-#					createcustommetadatasql = "select ElementName from CACValues where ElementName = '|#{full_path}|' and CreateCustomMetadata = -1"
-#					metadatarecords = db.query(createcustommetadatasql)
-#					metadatarecords.each do |metadatarecord|
-#						if elementvalue.strip != '' && elementvalue.strip != nil && elementvalue.to_s.length < 256
-#							case_path = full_path.strip
-#							begin
-#								worker_item.addCustomMetadata(full_path,elementvalue.to_s.strip,'text','user')
-#								csv << ["custom_metadata","#{full_path}",elementvalue.to_s.strip,Time.now.strftime("%d/%m/%Y")]
-#							rescue
-#								puts "ERROR 1 - Adding Custom Metadata #{full_path} - #{elementvalue} to #{worker_item.getSourceItem.getName}"
-#							end
-#						end
-#						elem.attributes.each_attribute do |attrs| 
-#                          if attrs.name != '' && attrs.name != nil && attrs.name.to_s.length < 256
-#								if attrs.value.to_s != '' && attrs.value.to_s != nil && attrs.value.to_s.length < 256
-#									basepath = path.map{|n|n.name}.join("/")
-#									begin
-#										worker_item.addCustomMetadata(full_path,attrs.value.to_s.strip,'text','user')
-#										csv << ["custom_metadata","#{full_path}",attrs.value.to_s.strip,Time.now.strftime("%d/%m/%Y")]
-#									rescue
-#										puts "ERROR 2 - Adding Custom Metadata #{full_path} - #{attrs.value.strip} to #{worker_item.getSourceItem.getName}"
-#									end
-#								end
-#							end
-#						end
-#					end
-#				end
-#			end
-#		end
-#	end
 end
