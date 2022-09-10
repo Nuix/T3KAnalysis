8880a69a96bfa9b828b8c375225855985b9a9230
Steven Joseph Luke <steven.luke@nuix.com>
2022-09-10 17:00:43 -0400

    Adding in a central, generic place to log

--- T3KAnalysis/t3k_api.rb
+++ T3KAnalysis/t3k_api.rb
@@ -1,5 +1,5 @@
 require 'json'
-require_relative 'rest_client/rest_client'
+require_relative 'utilities/rest_client'
 
 class T3kApi
   ENDPOINT_UPLOAD = "/upload"
@@ -62,9 +62,11 @@ class T3kApi
     @retry_delay = config[CONFIG_SECTION][CONFIG_RETRY_DELAY]
   end
 
-  # Performs a task, retrying it if it fails.  The task is passed in as a block.  The block
-  # should return true if it completed successfully and false if it failed.  After the
-  # defined number of retries, the task will be aborted uncompleted.
+  # Performs a task, retrying it if it fails.
+  #
+  # The task is passed in as a block.  The block takes no parameters and should return true if it completed successfully
+  # and false if it failed.  If the block returns false the block will be re-called after a delay.  After the defined
+  # number of retries, the task will be aborted uncompleted.
   def do_with_retries(&block)
     attempts = 0
 
@@ -80,10 +82,36 @@ class T3kApi
     end
 
     if do_try
-      puts "Exceeded #{@retry_count} retry attempts.  Task not completed."
+      raise "Exceeded #{@retry_count} retry attempts.  Task not completed."
     end
   end
 
+  # Upload a single file to the server and begin analyzing it.
+  #
+  # This endpoint is poorly named - the file must already exist on the server and this endpoint will simply tell the
+  # server to begin working on it.  It would be better named "start_analysis" but I am keeping the method name here
+  # consistent with the endpoint name (even though there is no 'upload' happening).
+  #
+  # The provided id must be a unique identifier for a running instance of the server.  It can be an integer or a String
+  # that contains an integer (such as an_integer.to_s, or a string that could successfully be turned into an integer
+  # with a_string.to_i).
+  #
+  # The provided path needs to be an absolute path on the server.  The host operating system is mounted on the server
+  # under /host/host_mnt/ but the contents of that mount seem to have limited visibility - a file will pass the
+  # first "exists" check made with this upload command, but will (could?) fail a later exists check when doing the
+  # analysis (in my tests it took close to 10s to fail that check).  It was safer to put the files under a dedicated
+  # mounting point.  I used something under /CORE/resources since that space was already used and visible by T3K.
+  #
+  # The upload request will be repeated if it fails with a 5xx error.  If it fails with a bad source id, the id will
+  # be incremented and tried again.
+  #
+  # @raise Exceptions if there was a malformed request, or it took too many retries to complete.
+  # @param [Int | String] id Unique identifier for the item to upload.  This is the request id, and will be different
+  #                           than the results id.
+  # @param [String] path Absolute path to the file as it exists on the server.  This path must be visible to the server
+  #                      and seems to be best located under the /CORE mount point (at least under the /host seemed to
+  #                      be only partially accessible to the server).
+  # @return The results id - the unique identifier for polling status and getting results for this item.
   def upload(id, path)
     result_id = nil
 
@@ -123,6 +151,29 @@ class T3kApi
     result_id
   end
 
+  # Upload a series of items to analyze in a single batch.
+  #
+  # See the #upload(id, path) method for details.  The key in the Hash parameter for this method is the unique source id
+  # for the item, and the value in the Hash is its path.
+  #
+  # @param [Hash<Int|String, String>] hash_of_items A Hash of items to upload, with source ids as keys for paths.  See
+  #                                                 the documents for #upload(id, path) for details on the id and path
+  #                                                 values.
+  # @return A Hash with the results ids for each item as values, keyed to the source id that was sent to this method.
+  # @see #upload(id, path)
+  def batch_upload(hash_of_items)
+
+  end
+
+
+  # Perform a single poll attempt for the status of an item being processed.
+  #
+  # This poll attempt will be retried if there are server errors, will throw exceptions if request was malformed or
+  # the file being processed was not found, and will return an error if the original image was found but broken.
+  #
+  # @todo Double check the errors here - perhaps be more permissive esp. of missing files
+  # @param [Int | String] id The results id to check status for
+  # @return The body of the poll request as a Hash
   def poll(id)
     poll_results = nil
 
@@ -166,6 +217,13 @@ class T3kApi
     poll_results
   end
 
+  # Repeatedly poll an item until it completes.
+  #
+  # This method uses #poll(id) to get the status and waits for it to reach the finished status.
+  #
+  # @param [Int | String] id The results id for the item to wait for
+  # @return A String with the results: "DONE" if finished without error, or an appropriate error message.
+  # @see #poll(id)
   def wait_for_analysis(id)
     done = false
     results = nil
@@ -199,6 +257,16 @@ class T3kApi
     results
   end
 
+  # Retrieve the results of analysis for an item.
+  #
+  # This function should be called only after the polling has returned in a finished state and without errors (if
+  # using the #wait_for_analysis(id) method, call this after it returns "DONE").
+  #
+  # Getting the results will be retried if there is a server error, and an exception will be thrown if there is a
+  # malformed request.
+  #
+  # @param [Int | String] id The results id for the items whose results will be retrieved
+  # @return A Hash with the results of the analysis
   def get_results(id)
     analysis_results = nil
 
---
+++ T3KAnalysis/utilities/multi_logger.rb
@@ -0,0 +1,61 @@
+require 'singleton'
+class MultiLogger
+  include Singleton
+
+  def initialize
+    @outputs = []
+    @logger = Logger.new(File.new('/dev/null'))
+    @logger.formatter = proc do | severity, datetime, progname, msg |
+      message = "#{datetime}: [#{progname}] #{severity.upcase} :: #{msg}"
+      @outputs.each do | output |
+        if output.class.method_defined? severity
+          output.send severity, msg
+        elsif output.class.method_defined? :puts
+          output.puts message
+        elsif output.class.method_defined? :write
+          output.write message
+        elsif output.class.method_defined? :log
+          output.log message
+        end
+      end
+    end
+  end
+
+  def add_output(logger)
+    @outputs.append logger
+  end
+
+  def close
+    @outputs.each do | logger |
+      if logger.class.method_defined? :close
+        logger.close
+      end
+    end
+
+    @logger.close
+  end
+
+  def debug(message)
+    @logger.debug message
+  end
+
+  def info(message)
+    @logger.info message
+  end
+
+  def warn(message)
+    @logger.warn message
+  end
+
+  def error(message)
+    @logger.error message
+  end
+
+  def fatal(message)
+    @logger.fatal message
+  end
+
+  def unknown(message)
+    @logger.unknown message
+  end
+end
